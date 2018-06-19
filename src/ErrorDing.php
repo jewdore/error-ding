00c3a0a0da6300d0b5d6c7cfa11aa6288d4ac95b
jewdore <jewdore@gmail.com>
2018-06-19 14:43:23 +0800

    init

---
+++ src/ErrorDing.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jewdore
+ * Date: 2018/6/19
+ * Time: 下午2:24
+ */
+
+namespace Jewdore\ErrorDing;
+
+
+class ErrorDing
+{
+    public function test()
+    {
+        echo "fdfdfd";
+    }
+}
